1dfd97a47c6a5018e21a18f10e3888c3a1187ec1
MQuan Pang <pangmiaoquan@wdxglobal.cn>
2026-01-26 10:24:59 +0800

    修复 for 循环语法错误

--- quan/1.c
+++ quan/1.c
@@ -5,7 +5,7 @@ int main(int argc, char**argv)
     printf("hello world!\n");
 
     if (argc > 1) {
-        for1 (int i = 1; i < argc; i++) {
+        for (int i = 1; i < argc; i++) {
             printf("Argument: %s\n", argv[i]);
         }
     }
